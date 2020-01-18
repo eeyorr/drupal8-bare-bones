3a698cda0714562f3d2ba1c380140c169e0bc7c7
eeyorr <godsbluehills@gmail.com>
2020-01-18 13:26:34 -0500

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Code for custom js.
+ */
+
+(function($, Drupal, drupalSettings) {
+  Drupal.behaviors.bare_bones = {
+      attach: function(context, settings) {
+
+        // Scripts go here
+
+      }
+    };
+})(jQuery, Drupal, drupalSettings);
